6f9eb353764980ef4411f2b032756edddb0ccd54
Sérgio Vitarelli Silva <sergiovitarellisilva@gmail.com>
2020-08-02 18:54:01 -0300

    adds tests

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sergio-silva/hello-world-go
 
 go 1.14
+
+require github.com/google/go-cmp v0.5.1
--- hello.go
+++ hello.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/google/go-cmp/cmp"
 	"github.com/sergio-silva/hello-world-go/morestrings"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello, Go"))
 }
