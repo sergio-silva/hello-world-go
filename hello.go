ceec97cad8762d5c23ac1d7c129ac62e32d80499
Sérgio Vitarelli Silva <sergiovitarellisilva@gmail.com>
2020-08-02 18:27:15 -0300

    add morestrings package

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sergio-silva/hello-world-go/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH."))
 }
